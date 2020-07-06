847ddad75e52f9dd59d613d5e12667b242b6491d
francois  samin <francois.samin@corp.ovh.com>
2020-07-06 11:26:07 +0200

    feat: remove nested directory limit
    Signed-off-by: francois  samin <francois.samin@corp.ovh.com>

--- provider_filetree.go
+++ provider_filetree.go
@@ -56,18 +56,38 @@ func browseDir(items []Item, path, basename string) ([]Item, error) {
 	}
 
 	for _, f := range files {
+		keyName := transformKey(basename)
+
 		filename := filepath.Join(path, f.Name())
 		if f.IsDir() {
-			return items, fmt.Errorf("subdir %s: encountered nested directory %s, max 1 level of nesting", basename, f.Name())
+			var subItems []Item
+			subItems, err = browseDir(subItems, filename, filepath.Join(basename, f.Name()))
+			if err != nil {
+				return nil, err
+			}
+			for _, subItem := range subItems {
+				i := newItem(keyName, subItem.value)
+				if !contains(items, i) {
+					items = append(items, i)
+				}
+			}
+			items = append(items, subItems...)
+			continue
 		}
-		it, err := readItem(filename, f.Name())
+
+		it1, err := readItem(filename, f.Name())
 		if err != nil {
 			return items, err
 		}
-		it.key = transformKey(basename)
-		items = append(items, it)
-		it.key = transformKey(filepath.Join(basename, f.Name()))
-		items = append(items, it)
+		it1.key = keyName
+
+		items = append(items, it1)
+
+		keyName2 := transformKey(filepath.Join(basename, f.Name()))
+		it2 := newItem(keyName2, it1.value)
+
+		it1.key = keyName2
+		items = append(items, it2)
 	}
 
 	return items, nil
@@ -78,10 +98,23 @@ func readItem(path, basename string) (Item, error) {
 	if err != nil {
 		return Item{}, err
 	}
+	return newItem(basename, string(content)), nil
+}
+
+func newItem(name, content string) Item {
 	priority := int64(5)
-	first, _ := utf8.DecodeRuneInString(basename)
+	first, _ := utf8.DecodeRuneInString(name)
 	if unicode.IsUpper(first) {
 		priority = 10
 	}
-	return Item{value: string(content), priority: priority}, nil
+	return Item{key: transformKey(name), value: content, priority: priority}
+}
+
+func contains(items []Item, i Item) bool {
+	for x := range items {
+		if items[x].key == i.key && items[x].value == i.value {
+			return true
+		}
+	}
+	return false
 }
--- provider_filetree_test.go
+++ provider_filetree_test.go
@@ -1,6 +1,7 @@
 package configstore
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,20 +12,29 @@ func TestFileTreeProvider(t *testing.T) {
 	var s = NewStore()
 	s.FileTree("tests/fixtures/filetreeprovider")
 	l, err := s.GetItemList()
+	require.NoError(t, err)
 
-	assert.Equal(t, 6, l.Len())
+	assert.Equal(t, 9, l.Len())
+
+	for _, i := range l.Items {
+		t.Logf("items: %s - %s", i.key, i.value)
+	}
 
 	barItems, has := l.indexed["bar"]
 	require.True(t, has, "missing 'bar' items")
-	require.Len(t, barItems, 2, "there must be 2 'bar' items")
+	require.Len(t, barItems, 3, "there must be 3 'bar' items")
 
-	biz := barItems[0]
-	require.Equal(t, "bar", biz.Key())
-	require.Equal(t, "biz value", biz.value)
+	bar := barItems[0]
+	require.Equal(t, "bar", bar.Key())
+	require.Equal(t, "baz_foo_value", bar.value)
 
-	buz := barItems[1]
-	require.Equal(t, "bar", buz.Key())
-	require.Equal(t, "buz value", buz.value)
+	bar = barItems[1]
+	require.Equal(t, "bar", bar.Key())
+	require.Equal(t, "biz value", bar.value)
+
+	bar = barItems[2]
+	require.Equal(t, "bar", bar.Key())
+	require.Equal(t, "buz value", bar.value)
 
 	bar_bizItems, has := l.indexed["bar/biz"]
 	require.True(t, has, "missing 'bar/biz' items")
@@ -58,5 +68,44 @@ func TestFileTreeProvider(t *testing.T) {
 	require.Equal(t, "foo", foo.Key())
 	require.Equal(t, "foo value", foo.value)
 
-	require.NoError(t, err)
+	barbazItems, has := l.indexed["bar/barbaz"]
+	require.True(t, has, "missing 'bar/barbaz' items")
+	require.Len(t, fooItems, 1, "there must be 1 'bar/barbaz' item")
+
+	barbaz_fooValue := barbazItems[0]
+	require.Equal(t, "bar/barbaz", barbaz_fooValue.Key())
+	require.Equal(t, "baz_foo_value", barbaz_fooValue.value)
+
+	barbaz_fooItems, has := l.indexed["bar/barbaz/foo"]
+	require.True(t, has, "missing 'bar/barbaz/foo' items")
+	require.Len(t, fooItems, 1, "there must be 1 'bar/barbaz/foo' item")
+
+	barbaz_fooValue = barbaz_fooItems[0]
+	require.Equal(t, "bar/barbaz/foo", barbaz_fooValue.Key())
+	require.Equal(t, "baz_foo_value", barbaz_fooValue.value)
+
+}
+
+func ExampleStore_FileTree() {
+	var s = NewStore()
+	s.FileTree("tests/fixtures/filetreeprovider2")
+	l, err := s.GetItemList()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, i := range l.Items {
+		fmt.Printf("%s - %s\n", i.key, i.value)
+	}
+	// Output:
+	// database - buz value
+	// database - fiz value
+	// database/dev - buz value
+	// database/dev/buz - buz value
+	// database/dev - fiz value
+	// database/dev/fiz - fiz value
+	// database - prod foo value
+	// database/prod - prod foo value
+	// database/prod/foo - prod foo value
+	// foo - foo value
 }
