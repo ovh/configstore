863ccbd584dec695cd9df109048785d017deba72
Romain Beuque <556072+rbeuque74@users.noreply.github.com>
2023-08-30 17:16:33 +0000

    fix: remove deprecated ioutil.ReadFile and switch to os.ReadFile
    Signed-off-by: Romain Beuque <556072+rbeuque74@users.noreply.github.com>

--- providers.go
+++ providers.go
@@ -168,7 +168,7 @@ func fileList(s *Store, dirname string, refresh bool) {
 
 func readFile(filename string, fn func([]byte) ([]Item, error)) ([]Item, error) {
 	vals := []Item{}
-	b, err := ioutil.ReadFile(filename)
+	b, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
